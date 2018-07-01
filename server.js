960aead012d6fdca606826b238a2ec325e14f05f
Rungtip Pronaiam <rungtip.rtpa@gmail.com>
2018-07-01 11:07:54 +0700

    แก้พอร์ต

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3001, () => {
+server.listen(3000, () => {
  console.log("JSON Server is running");
 });
 
